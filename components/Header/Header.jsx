78c248bb6ece8caba0f20afc9aac1299ec31379f
Venkata Lohith Kumar Reddy Mukkamalla <lohithreddym4@gmail.com>
2025-02-05 17:41:53 +0530

    changes

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -12,9 +12,9 @@ const Header = () => {
    <header>
     <nav className="navbar navbar-expand-lg navigation fixed-top bg-light" id="navbar">
      <div className="container">
-      <a className="navbar-brand d-flex align-items-center" href="index.html">
+      <a className="navbar-brand d-flex align-items-center" href="/">
        <img src="images/logo.png" alt="" className="img-fluid" />
-       <p className='logo-title mb-0 p-0'>MAZHARULLAH.</p>
+       <p className='logo-title mb-0 p-0'>Dr. MAZHARULLAH</p>
       </a>
 
       <button className={`navbar-toggler ${isNavOpen ? '' : 'collapsed'}`} type="button" onClick={toggleNav} aria-label="Toggle navigation">
