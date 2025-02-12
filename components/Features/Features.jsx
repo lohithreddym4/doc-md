9df195ecba801225da88619baaa71433a22acbc5
Venkata Lohith Kumar Reddy Mukkamalla <lohithreddym4@gmail.com>
2025-02-12 17:47:21 +0530

    timings

--- components/Features/Features.jsx
+++ components/Features/Features.jsx
@@ -13,28 +13,24 @@ const Features = () => {
           <i className="icofont icofont-surgeon-alt"></i>
          </div>
          <span>24 Hours Service</span>
-         <h4 className="mb-3">Online Appoinment</h4>
+         <h4 className="mb-3">Online Appointment</h4>
          <p className="mb-4">We've implemented the principle of family medicine, ensuring continuous care and support for you and your loved ones. Give us a call at +91 9948140654</p>
-         </div>
+        </div>
 
         <div className="feature-item mb-5 mb-lg-0">
          <div className="feature-icon mb-4">
           <i className="icofont icofont-ui-clock"></i>
          </div>
-         <span>Timing schedule</span>
-         <h4 className="mb-3">Working Hours</h4>
-         <ul className="w-hours list-unstyled">
-          <li className="d-flex justify-content-between">Sun - Wed : <span>7:00 - 18:00</span></li>
-          <li className="d-flex justify-content-between">Thu - Fri : <span>8:00 - 18:00</span></li>
-          <li className="d-flex justify-content-between">Sat - sun : <span>9:00 - 18:00</span></li>
-         </ul>
+         <span>Expert Medical Care</span>
+         <h4 className="mb-3">Dr. Mohammad Mazharullah</h4>
+         <p>Dr. Mohammad Mazharullah, MBBS (OSM), MD in General Medicine, is a senior consultant physician and diabetologist.</p>
         </div>
 
         <div className="feature-item mb-5 mb-lg-0">
          <div className="feature-icon mb-4">
           <i className="icofont icofont-support"></i>
          </div>
-         <span>Emegency Cases</span>
+         <span>Emergency Cases</span>
          <h4 className="mb-3">+91 9948140654</h4>
          <p>Experience all-time support for emergencies. We embrace the principle of family medicine, ensuring continuous care. Connect with us for any urgent need.</p>
         </div>
